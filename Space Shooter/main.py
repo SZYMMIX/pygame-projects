e75c9b590a7287e941f372925cebc0477d2182e4
Szymon <szycim10@gmail.com>
2025-10-16 02:24:50 +0200

    Set game's window up

---
+++ Space Shooter/main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
