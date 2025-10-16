3c4a238ae9d8f98290fc17643404df7f2a443deb
Szymon <szycim10@gmail.com>
2025-10-16 03:22:58 +0200

    Add stars and rectangles

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from os.path import join
 
 pygame.init()
@@ -6,14 +7,20 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
 pygame.display.set_caption('Space Shooter')
-path = join('Space Shooter', 'Assets', 'images', 'player.png')
-player_surf = pygame.image.load(path).convert_alpha()
+player_surf = pygame.image.load(join('Space Shooter', 'Assets', 'images', 'player.png')).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+star_surf = pygame.image.load(join('Space Shooter', 'Assets', 'images', 'star.png')).convert_alpha()
+star_positions = [(random.randint(0, 1180), random.randint(0, 620)) for _ in range(20)]
 
+generated = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     display_surface.fill("darkgray")
-    display_surface.blit(player_surf, (100, 100))
+    for star_pos in star_positions:
+        display_surface.blit(star_surf, star_pos)
+    player_rect.left += 0.1
+    display_surface.blit(player_surf, player_rect)
     pygame.display.update()
 pygame.quit()
