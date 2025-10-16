8e6c65c8da3a6a5c6e8f5852647001ab1ebd546b
Szymon <szycim10@gmail.com>
2025-10-16 02:58:08 +0200

    Add player model

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -1,15 +1,19 @@
 import pygame
+from os.path import join
 
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
 pygame.display.set_caption('Space Shooter')
+path = join('Space Shooter', 'Assets', 'images', 'player.png')
+player_surf = pygame.image.load(path).convert_alpha()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    display_surface.fill("red")
+    display_surface.fill("darkgray")
+    display_surface.blit(player_surf, (100, 100))
     pygame.display.update()
 pygame.quit()
