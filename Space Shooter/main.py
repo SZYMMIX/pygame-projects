ef8ee67aac9fe30bf8e1a2d4c9edcd802af0bb07
Szymon <szycim10@gmail.com>
2025-10-16 04:13:26 +0200

    Fix player movement. Make player move like DVD logo

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -6,18 +6,26 @@ pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
+clock = pygame.time.Clock()
+
 pygame.display.set_caption('Space Shooter')
 player_surf = pygame.image.load(join('Space Shooter', 'Assets', 'images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = pygame.math.Vector2(1, 1)
+player_speed = 300
+
 star_surf = pygame.image.load(join('Space Shooter', 'Assets', 'images', 'star.png')).convert_alpha()
 star_positions = [(random.randint(0, 1180), random.randint(0, 620)) for _ in range(20)]
+
 meteor_surf = pygame.image.load(join('Space Shooter', 'Assets', 'images', 'meteor.png')).convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
 laser_surf = pygame.image.load(join('Space Shooter', 'Assets', 'images', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
-right = True
+
 while running:
+    dt = clock.tick() / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -26,16 +34,13 @@ while running:
     for star_pos in star_positions:
         display_surface.blit(star_surf, star_pos)
 
-    if right:
-        player_rect.left += 0.2
-    else:
-            player_rect.right -= 0.2
+    player_rect.center += player_direction * player_speed * dt
 
-    if player_rect.right >= WINDOW_WIDTH:
-        right = False
-
-    if player_rect.left <= 0:
-        right = True
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction.x *= -1
+    
+    if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
+        player_direction.y *= -1
 
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
