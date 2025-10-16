e12a4f80b65be6a4308b8328f2fbe78a93eb3c77
Szymon <szycim10@gmail.com>
2025-10-16 02:38:55 +0200

    Add assets and change the background color

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -4,8 +4,12 @@ pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 running = True
+pygame.display.set_caption('Space Shooter')
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    display_surface.fill("red")
+    pygame.display.update()
+pygame.quit()
