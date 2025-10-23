5b161f26044eba66b8cc51a8929f599bea7bc48a
Szymon <szycim10@gmail.com>
2025-10-23 03:16:59 +0200

    Change player movement and add player input

--- Space Shooter/main.py
+++ Space Shooter/main.py
@@ -11,7 +11,7 @@ clock = pygame.time.Clock()
 pygame.display.set_caption('Space Shooter')
 player_surf = pygame.image.load(join('Space Shooter', 'Assets', 'images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(1, 1)
+player_direction = pygame.math.Vector2()
 player_speed = 300
 
 star_surf = pygame.image.load(join('Space Shooter', 'Assets', 'images', 'star.png')).convert_alpha()
@@ -29,18 +29,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT]) 
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP]) 
+    player_direction = player_direction.normalize() if player_direction else player_direction
+
     display_surface.fill("darkgray")
 
     for star_pos in star_positions:
         display_surface.blit(star_surf, star_pos)
 
     player_rect.center += player_direction * player_speed * dt
-
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction.x *= -1
-    
-    if player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
-        player_direction.y *= -1
 
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
