40458432ccd75714d872891b2992e4eb6a8f2598
Szymon Cimochowski <szycim10@gmail.com>
2025-11-20 01:51:29 +0100

    Implement player animations

--- Spellbound Ramblers/player.py
+++ Spellbound Ramblers/player.py
@@ -3,6 +3,9 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups, collision_sprites, pos):
         super().__init__(groups)
+        self.load_images()
+        self.state = 'down'
+        self.frame_index = 0
         self.image = pygame.image.load(join('Spellbound Ramblers', 'Assets', 'images', 'player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
         self.hitbox_rect = self.rect.inflate(-60, -80)
@@ -10,6 +13,17 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2()
         self.speed = 400
         self.collision_sprites = collision_sprites
+    
+    def load_images(self):
+        self.frames = {'left': [], 'right': [], 'up': [], 'down': []}
+
+        for state in self.frames.keys():
+            for folder_path, sub_folders, file_names in walk(join('Spellbound Ramblers', 'Assets', 'images', 'player', state)):
+                if file_names:
+                    for file_name in sorted(file_names, key= lambda name: int(name.split('.')[0])):
+                        full_path = join(folder_path, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
 
     def _handle_movement(self):
         keys = pygame.key.get_pressed()
@@ -25,9 +39,23 @@ class Player(pygame.sprite.Sprite):
         self.collision('vertical')
         self.rect.center = self.hitbox_rect.center
 
+    def animate(self, dt):
+        
+
+        if self.direction.x != 0:
+            self.state = 'right' if self.direction.x > 0 else 'left'
+        
+        if self.direction.y != 0:
+            self.state = 'down' if self.direction.y > 0 else 'up'
+
+        self.frame_index = self.frame_index + 6 * dt if self.direction else 0
+        self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
+
+
     def update(self, dt):
         self._handle_movement()
         self._move(dt)
+        self.animate(dt)
         
 
     def collision(self, direction):
